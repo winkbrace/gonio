f0632fdc664110f6959105111677f60da8ebcd8a
Bas de Ruiter <deruiterb@gmail.com>
2024-03-17 19:58:38 +0100

    Draw line perpendicular to a circle in the direction of the mouse

---
+++ script.js
@@ -0,0 +1,41 @@
+const canvasBg = document.getElementById('background');
+const bg = canvasBg.getContext("2d");
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext("2d");
+ctx.lineWidth = 2;
+
+const center = {x: canvas.width / 2, y: canvas.height / 2}
+const radius = canvas.height / 2 - 150;
+
+bg.lineWidth = 2
+bg.beginPath();
+bg.arc(center.x, center.y, radius, 0, 2 * Math.PI);
+bg.strokeStyle = 'yellow'
+bg.stroke();
+
+
+document.addEventListener('mousemove', function(evt) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+
+    const rect = canvas.getBoundingClientRect();
+    const x = evt.clientX - rect.left;
+    const y = evt.clientY - rect.top;
+
+    // find intersection with circle
+    const dx = x - center.x;
+    const dy = y - center.y;
+    const angle = Math.atan2(dy, dx); // tan(a) = O / A
+    const intersectionX = center.x + radius * Math.cos(angle);
+    const intersectionY = center.y + radius * Math.sin(angle);
+    ctx.moveTo(center.x, center.y);
+    ctx.lineTo(intersectionX, intersectionY);
+
+    // draw line perpendicular
+    ctx.moveTo(intersectionX + 1000 * Math.sin(angle), intersectionY - 1000 * Math.cos(angle));
+    ctx.lineTo(intersectionX - 1000 * Math.sin(angle), intersectionY + 1000 * Math.cos(angle));
+
+    ctx.strokeStyle = 'orange'
+    ctx.stroke();
+});
